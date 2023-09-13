375074f41bfffbc492442012826131b8297dc162
elhm88 <elhm88@student.cis.edu.hk>
2023-09-13 09:44:15 +0800

    Quiz 1.1 completed.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your first name: ");
+        firstName = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Enter your favourite food: ");
+        favouriteFood = scanner.nextLine();
+
+        System.out.println("\nFirst name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
 
 
     }
